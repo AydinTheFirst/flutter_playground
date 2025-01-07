43b3b351632ac55b89fd38e3a731f46c554c1312
Halil Aydın <84444757+AydinTheFirst@users.noreply.github.com>
2025-01-07 15:51:40 +0300

    Add initial Flutter counter app implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
+void main() {
+  runApp(
+    MaterialApp(
+      home: MyApp(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+        ),
+      ),
+    ),
+  );
+}
+
+class MyApp extends HookWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final counter = useState(0);
+
+    void increment() {
+      counter.value++;
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        foregroundColor: Theme.of(context).colorScheme.onPrimary,
+        title: Text('Counter App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '${counter.value}',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: increment,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
